b86b9fc4e12dc5f5a0996b58727f921aee99fffd
Helder Boone <helder.boone@gmail.com>
2020-02-06 15:43:26 -0300

    Create first database migration

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -7,8 +7,6 @@ namespace DatingApp.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-        public DbSet<Value> Values { get; set; }
-
-        
+        public DbSet<Value> Values { get; set; }        
     }
 }
