a6005879afb815bc783bba519f076be1ce1c859e
Helder Boone <helder.boone@gmail.com>
2020-03-03 16:01:14 -0300

    Use jwt library to check whether the token is valid or not

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -10,7 +10,7 @@ import { AlertifyServiceService } from '../_services/alertify-service.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyServiceService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyServiceService) { }
 
   ngOnInit() {
   }
@@ -24,8 +24,7 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
