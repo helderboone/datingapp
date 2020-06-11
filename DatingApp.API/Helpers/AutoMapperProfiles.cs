e5a35b348c3b520350d5388057cbe05263df95af
Helder Boone <helder.boone@gmail.com>
2020-06-11 18:29:39 -0300

    Create update user feature

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -21,6 +21,7 @@ namespace DatingApp.API.Helpers
                 .ForMember(dest => dest.Age, opt => 
                     opt.MapFrom(src => src.DateofBirth.CalculateAge()));
             CreateMap<Photo, PhotoForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
