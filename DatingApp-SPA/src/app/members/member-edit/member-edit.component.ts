ac02ff8826b9da88f8bb2eca3927523ab4084bd4
Helder Boone <helder.boone@gmail.com>
2020-06-11 17:57:06 -0300

    Add a candeactivate route guard

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 import { User } from 'src/app/_models/user';
 import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from 'src/app/_services/alertify.service';
@@ -10,10 +10,16 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
-  @ViewChild('editForm') editForm: NgForm;
-  user: User;
   constructor(private route: ActivatedRoute,
               private alertify: AlertifyService) { }
+  @ViewChild('editForm') editForm: NgForm;
+  user: User;
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -26,4 +32,5 @@ export class MemberEditComponent implements OnInit {
     this.alertify.success('Profile updated successfully');
     this.editForm.reset(this.user);
   }
+
 }
