cfe5f53e978b8a486de105e36567ae8e699a4fda
Helder Boone <helder.boone@gmail.com>
2020-06-30 18:33:39 -0300

    Create and get photo by user Id

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -22,6 +22,8 @@ namespace DatingApp.API.Helpers
                     opt.MapFrom(src => src.DateofBirth.CalculateAge()));
             CreateMap<Photo, PhotoForDetailedDto>();
             CreateMap<UserForUpdateDto, User>();
+            CreateMap<Photo, PhotoForReturnDto>();
+            CreateMap<PhotoForCreationDto, Photo>();            
         }
     }
 }
