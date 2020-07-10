4fd41a0f95f605899398ae3c2149a8aa759a910c
Helder Boone <helder.boone@gmail.com>
2020-07-10 11:24:25 -0300

    Set file without credentials since it uses JWT

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -35,6 +35,7 @@ export class PhotoEditorComponent implements OnInit {
       autoUpload: false,
       maxFileSize: 10 * 1024 * 1024 // 10 Mb
     });
+    this.uploader.onAfterAddingFile = (file) => {file.withCredentials  = false; };
   }
 
 }
