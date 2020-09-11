4941361cccc7ab8d5ecbf79bb928a47e5625e891
Helder Boone <helder.boone@itixti.com.br>
2020-09-10 22:07:52 -0300

    Update the register method in the api

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,8 +1,3 @@
-using System;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
@@ -10,79 +5,83 @@ using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DatingApp.API.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class AuthController : ControllerBase
-  {
-    private readonly IAuthRepository _repo;
-    private readonly IConfiguration _configuration;
-    private readonly IMapper _mapper;
-
-    public AuthController(IAuthRepository repo, IConfiguration configuration, IMapper mapper)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
     {
-      _mapper = mapper;
-      _repo = repo;
-      _configuration = configuration;
-    }
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
 
-    [HttpPost("register")]
-    public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
-    {
-      //validate request
+        public AuthController(IAuthRepository repo, IConfiguration configuration, IMapper mapper)
+        {
+            _mapper = mapper;
+            _repo = repo;
+            _configuration = configuration;
+        }
 
-      userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            //validate request
 
-      if (await _repo.UserExists(userForRegisterDto.Username))
-        return BadRequest("Username already exists");
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-      var userToCreate = new User
-      {
-        Username = userForRegisterDto.Username
-      };
+            if (await _repo.UserExists(userForRegisterDto.Username))
+                return BadRequest("Username already exists");
 
-      var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+            var userToCreate = _mapper.Map<User>(userForRegisterDto);
 
-      return StatusCode(201);
-    }
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
-    [HttpPost("login")]
-    public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
-    {
-      var userFromRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
+            var userToReturn = _mapper.Map<UserForDetailedDto>(createdUser);
 
-      if (userFromRepo == null) return Unauthorized();
+            return CreatedAtRoute("GetUser", new { controller = "Users", id = createdUser.Id }, userToReturn);
+        }
 
-      var claims = new[]
-      {
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
+
+            if (userFromRepo == null) return Unauthorized();
+
+            var claims = new[]
+            {
             new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
             new Claim(ClaimTypes.Name, userFromRepo.Username)
           };
 
-      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
 
-      var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
-      var tokenDescriptor = new SecurityTokenDescriptor
-      {
-        Subject = new ClaimsIdentity(claims),
-        Expires = DateTime.Now.AddDays(1),
-        SigningCredentials = creds
-      };
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
 
-      var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenHandler = new JwtSecurityTokenHandler();
 
-      var token = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.CreateToken(tokenDescriptor);
 
-      var user = _mapper.Map<UserForListDto>(userFromRepo);
+            var user = _mapper.Map<UserForListDto>(userFromRepo);
 
-          return Ok(new 
-          { 
-            token = tokenHandler.WriteToken(token),
-            user
-          });
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token),
+                user
+            });
+        }
     }
-  }
 }
--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -1,7 +1,7 @@
-using System.Linq;
 using AutoMapper;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
+using System.Linq;
 
 namespace DatingApp.API.Helpers
 {
@@ -10,20 +10,21 @@ namespace DatingApp.API.Helpers
         public AutoMapperProfiles()
         {
             CreateMap<User, UserForListDto>()
-                .ForMember(dest => dest.PhotoUrl, opt => 
+                .ForMember(dest => dest.PhotoUrl, opt =>
                     opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
-                .ForMember(dest => dest.Age, opt => 
+                .ForMember(dest => dest.Age, opt =>
                     opt.MapFrom(src => src.DateofBirth.CalculateAge()));
-                    
+
             CreateMap<User, UserForDetailedDto>()
-                .ForMember(dest => dest.PhotoUrl, opt => 
+                .ForMember(dest => dest.PhotoUrl, opt =>
                     opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
-                .ForMember(dest => dest.Age, opt => 
+                .ForMember(dest => dest.Age, opt =>
                     opt.MapFrom(src => src.DateofBirth.CalculateAge()));
             CreateMap<Photo, PhotoForDetailedDto>();
             CreateMap<UserForUpdateDto, User>();
             CreateMap<Photo, PhotoForReturnDto>();
-            CreateMap<PhotoForCreationDto, Photo>();            
+            CreateMap<PhotoForCreationDto, Photo>();
+            CreateMap<UserForRegisterDto, User>();
         }
     }
 }
